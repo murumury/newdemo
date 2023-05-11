02332a0057d956f29a049fbadc202cf024e16985
murumury <314160203@qq.com>
2023-05-11 13:44:10 +0800

    update

--- utils/presence.ts
+++ utils/presence.ts
@@ -3,11 +3,10 @@ import { createPresence } from '@yomo/presence';
 export function connect() {
   const id = Math.random().toString().substring(2, 10);
   const avatar = Math.random() > 0.5 ? `https://robohash.org/${id}` : void 0;
-  const presencePromise = createPresence({
-    url: process.env.NEXT_PUBLIC_PRESENCE_URL,
-    publicKey: process.env.NEXT_PUBLIC_PRESENCE_PUBLIC_KEY,
+  const presencePromise = createPresence(
+    'https://prscd2.allegro.earth/v1',
+    {publicKey: process.env.NEXT_PUBLIC_PRESENCE_PUBLIC_KEY,
     id,
-    appId: process.env.NEXT_PUBLIC_PRESENCE_APP_ID,
   });
   return { id, avatar, presencePromise };
 }
