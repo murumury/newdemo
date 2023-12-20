ed9df8269cd9fdd6709c2c136c4ffc289c1b56ed
murumury <314160203@qq.com>
2023-12-20 17:30:37 +0800

    fix presence

--- utils/presence.ts
+++ utils/presence.ts
@@ -1,13 +1,16 @@
 import { createPresence } from '@yomo/presence';
 
 export function connect() {
+  const presenceUrl = process.env.NEXT_PUBLIC_PRESENCE_URL;
+if (!presenceUrl) {
+    throw new Error('The NEXT_PUBLIC_PRESENCE_URL is not defined.');
+}
   const id = Math.random().toString().substring(2, 10);
   const avatar = Math.random() > 0.5 ? `https://robohash.org/${id}` : void 0;
-  const presencePromise = createPresence(process.env.NEXT_PUBLIC_PRESENCE_URL,{
+  const presencePromise = createPresence(presenceUrl,{
    
     publicKey: process.env.NEXT_PUBLIC_PRESENCE_PUBLIC_KEY,
     id,
-    appId: process.env.NEXT_PUBLIC_PRESENCE_APP_ID,
   });
   return { id, avatar, presencePromise };
 }
